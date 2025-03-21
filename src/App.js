01afeed7dec3621917c6214e0c95ee04cbbd06fb
noamgilad <noamgil@post.bgu.ac.il>
2025-03-21 23:37:23 +0200

    fix: update default route to use Register component instead of Login

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
